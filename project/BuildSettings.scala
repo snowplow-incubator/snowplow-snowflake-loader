e2a85dc8e62389ae8f155715c23ac28d98cb501a
Rostyslav Zatserkovnyi <zatserkovnyi.rostyslav@gmail.com>
2019-02-28 13:43:09 +0200

    Prepare for release

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -27,7 +27,7 @@ object BuildSettings {
 
   lazy val buildSettings = Seq[Setting[_]](
     name := "snowplow-snowflake-loader",
-    version := "0.4.1",
+    version := "0.4.2",
     organization := "com.snowplowanalytics",
     scalaVersion := "2.11.12",
     resolvers ++= Seq(
