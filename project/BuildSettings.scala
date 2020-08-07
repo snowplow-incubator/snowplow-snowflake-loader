30047cb15062cf6d0fe98aa0fc97467fc16f0733
Oguzhan Unlu <oguzhan@snowplowanalytics.com>
2020-08-07 13:02:28 +0300

    bump to rc5

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -27,7 +27,7 @@ object BuildSettings {
 
   lazy val buildSettings = Seq[Setting[_]](
     name := "snowplow-snowflake-loader",
-    version := "0.7.0-rc4",
+    version := "0.7.0-rc5",
     organization := "com.snowplowanalytics",
     scalaVersion := "2.11.12",
     resolvers ++= Seq(
