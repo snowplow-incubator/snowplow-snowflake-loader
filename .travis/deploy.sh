4e486fb7394c78b6a2e3dc64255bfd63681fafa0
Dilyan Damyanov <dilyand@gmail.com>
2021-05-06 13:28:21 +0100

    Migrate from Travis to Github Actions (close #135)

--- .travis/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tag=$1
-
-cd $TRAVIS_BUILD_DIR
-
-export TRAVIS_BUILD_RELEASE_TAG=${tag}
-release-manager --config ./.travis/release.yml --check-version --make-artifact --upload-artifact
