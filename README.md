5e8a57d6f90a096a46d4d6ef5b9ccf08a8dd19a7
Ian Streeter <ian@snowplowanalytics.com>
2024-11-26 12:20:37 +0000

    Change License to SLULA 1.1

--- README.md
+++ README.md
@@ -74,5 +74,5 @@ Licensed under the [Snowplow Limited Use License Agreement][license]. _(If you a
 [release-image]: https://img.shields.io/badge/release-0.2.4-blue.svg?style=flat
 [releases]: https://github.com/snowplow-incubator/snowplow-snowflake-streaming-loader/releases
 
-[license]: https://docs.snowplow.io/limited-use-license-1.0
+[license]: https://docs.snowplow.io/limited-use-license-1.1
 [license-image]: https://img.shields.io/badge/license-Snowplow--Limited--Use-blue.svg?style=flat
--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -2,8 +2,8 @@
  * Copyright (c) 2014-present Snowplow Analytics Ltd. All rights reserved.
  *
  * This software is made available by Snowplow Analytics, Ltd.,
- * under the terms of the Snowplow Limited Use License Agreement, Version 1.0
- * located at https://docs.snowplow.io/limited-use-license-1.0
+ * under the terms of the Snowplow Limited Use License Agreement, Version 1.1
+ * located at https://docs.snowplow.io/limited-use-license-1.1
  * BY INSTALLING, DOWNLOADING, ACCESSING, USING OR DISTRIBUTING ANY PORTION
  * OF THE SOFTWARE, YOU AGREE TO THE TERMS OF SUCH LICENSE AGREEMENT.
  */
@@ -39,14 +39,14 @@ object BuildSettings {
       IO.copyFile(file("LICENSE.md"), license)
       Seq(license)
     }.taskValue,
-    licenses += ("Snowplow Limited Use License Agreement", url("https://docs.snowplow.io/limited-use-license-1.0")),
+    licenses += ("Snowplow Limited Use License Agreement", url("https://docs.snowplow.io/limited-use-license-1.1")),
     headerLicense := Some(
       HeaderLicense.Custom(
         """|Copyright (c) 2014-present Snowplow Analytics Ltd. All rights reserved.
           |
           |This software is made available by Snowplow Analytics, Ltd.,
-          |under the terms of the Snowplow Limited Use License Agreement, Version 1.0
-          |located at https://docs.snowplow.io/limited-use-license-1.0
+          |under the terms of the Snowplow Limited Use License Agreement, Version 1.1
+          |located at https://docs.snowplow.io/limited-use-license-1.1
           |BY INSTALLING, DOWNLOADING, ACCESSING, USING OR DISTRIBUTING ANY PORTION
           |OF THE SOFTWARE, YOU AGREE TO THE TERMS OF SUCH LICENSE AGREEMENT.
           |""".stripMargin
