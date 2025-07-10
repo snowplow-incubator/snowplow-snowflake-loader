6d6c7884db5e7b9c3dbc4c2329d436a3d717e145
Ian Streeter <ian@snowplowanalytics.com>
2025-07-10 20:29:32 +0100

    Use offset tokens to ensure at-least-once semantics
    Until now, we have (effectively) used response codes from Snowflake to
    tell us that the events are safely in Snowflake. This was done via the
    snowflake java sdk; specifically, if `.flush()` returns with no error
    then we assumed the events are in snowflake.
    
    But we hit edge cases where snowflake would response with a successful
    response code, but the events were not fully ingested into the table.
    This edge case can happen around the time of schema evolution.
    
    After this commit, we use offset tokens to check the ingestion of a
    batch of events. We do not assume the batch is ingested to the table
    until `channel.getLatestCommittedOffsetToken()` returns the expected
    offset.

--- modules/kinesis/src/test/scala/com/snowplowanalytics/snowplow/snowflake/KinesisConfigSpec.scala
+++ modules/kinesis/src/test/scala/com/snowplowanalytics/snowplow/snowflake/KinesisConfigSpec.scala
@@ -111,8 +111,9 @@ object KinesisConfigSpec {
     ),
     cpuParallelismFactor = BigDecimal(0.75),
     retries = Config.Retries(
-      setupErrors     = Retrying.Config.ForSetup(delay = 30.seconds),
-      transientErrors = Retrying.Config.ForTransient(delay = 1.second, attempts = 5)
+      setupErrors          = Retrying.Config.ForSetup(delay = 30.seconds),
+      transientErrors      = Retrying.Config.ForTransient(delay = 1.second, attempts = 5),
+      checkCommittedOffset = Config.CheckCommittedOffsetRetries(delay = 100.millis)
     ),
     skipSchemas = List.empty,
     telemetry = Telemetry.Config(
@@ -191,8 +192,9 @@ object KinesisConfigSpec {
     ),
     cpuParallelismFactor = BigDecimal(0.75),
     retries = Config.Retries(
-      setupErrors     = Retrying.Config.ForSetup(delay = 30.seconds),
-      transientErrors = Retrying.Config.ForTransient(delay = 1.second, attempts = 5)
+      setupErrors          = Retrying.Config.ForSetup(delay = 30.seconds),
+      transientErrors      = Retrying.Config.ForTransient(delay = 1.second, attempts = 5),
+      checkCommittedOffset = Config.CheckCommittedOffsetRetries(delay = 100.millis)
     ),
     skipSchemas = List(
       SchemaCriterion.parse("iglu:com.acme/skipped1/jsonschema/1-0-0").get,
